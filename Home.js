6c32d0ef6ccf8794703159ee12025121032aeea4
changfuguo <changfuguo@qq.com>
2015-10-18 19:42:35 +0800

    add scorll-view plugin for home

--- Home.js
+++ Home.js
@@ -1,5 +1,78 @@
 'use strict';
 
-var react  = require('react-native');
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+} = React;
 
-var 
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+var HomeTabBar = require('./HomeTabBar');
+var deviceWidth = Dimensions.get('window').width;
+
+var CONST_BARS =[{
+		name: '电影',
+		value: 'movie',
+	},
+	{
+		name: '电视剧',
+		value: 'tv'
+	},{
+		name: '综艺',
+		value: 'zy'
+	},{
+		name: '动漫',
+		value: 'katong'
+	},{
+		name: '直播',
+		value: 'live'
+	}];
+var Home = React.createClass({
+		render() {
+			var bars = CONST_BARS.map((tab, i) => {
+				return (
+					<ScrollView tabLabel={tab.name} style={styles.tabView}>
+						<View style={styles.card}>
+								<Text>News</Text>
+						</View>
+					</ScrollView>	
+				);	
+			});
+
+			return (
+				<View style={styles.container}>
+					<ScrollableTabView renderTabBar={() => <HomeTabBar />}>
+						{bars}
+					</ScrollableTabView>
+			  </View>
+			);
+	  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabView: {
+    width: deviceWidth,
+    padding: 10,
+  },
+  card: {
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderColor: 'rgba(0,0,0,0.1)',
+    margin: 5,
+    height: 150,
+    padding: 15,
+    shadowColor: '#ccc',
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
+});
+
+module.exports = Home; 
---
+++ HomeTabBar.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Animated,
+} = React;
+
+var {Icon,} = require('react-native-icons');
+var deviceWidth = Dimensions.get('window').width;
+
+var styles = StyleSheet.create({
+	tab: {
+			flex: 1,
+			alignItems: 'center',
+			justifyContent: 'center',
+	},
+	container: {
+		height: 40,
+	},
+	tabs: {
+		height: 30,
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		backgroundColor:'#3a3941',
+		borderTopRadius: 3,
+		borderRightRadius: 3,
+	},
+	arrow :{
+		height:10,
+		width:10,
+		padding:0,
+		marginTop:-1,
+	}
+});
+
+var DefaultTabBar = React.createClass({
+  propTypes: {
+    goToPage: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    tabs: React.PropTypes.array
+  },
+
+  renderTabOption(name, page) {
+    var isTabActive = this.props.activeTab === page;
+
+    return (
+      <TouchableOpacity style={[styles.tab]} key={name} onPress={() => this.props.goToPage(page)}>
+        <View>
+          <Text style={{color: isTabActive ? '#fff' : '#999', fontWeight: isTabActive ? 'bold' : 'normal'}}>{name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  },
+   render() {
+    var numberOfTabs = this.props.tabs.length;
+    var tabUnderlineStyle = {
+      position: 'absolute',
+	  justfiyContent: 'center',
+	  alignItems: 'center',
+      width: deviceWidth / numberOfTabs,
+      height: 10,
+      top: 30,
+    };
+
+    var left = this.props.scrollValue.interpolate({
+      inputRange: [0, 1], outputRange: [0, deviceWidth / numberOfTabs]
+    });
+
+    return (
+		<View style={styles.container}>
+			<View style={styles.tabs}>
+				{this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
+			</View>
+			<Animated.View style={[tabUnderlineStyle, {left: left}]} >
+				<Icon name='fontawesome|play' color='rgba(0,0,0,.8)' size ={10} rotation={90} style={styles.arrow}/>
+			</Animated.View>
+		</View>
+    );
+  },
+});
+
+module.exports = DefaultTabBar;
